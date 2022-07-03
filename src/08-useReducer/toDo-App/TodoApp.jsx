793235fc01d21f5a3745b8fee41b680112bb75ff
Ferla04 <fer.velas135@gmail.com>
2022-07-02 21:56:38 -0500

    useTodo

--- src/08-useReducer/toDo-App/TodoApp.jsx
+++ src/08-useReducer/toDo-App/TodoApp.jsx
@@ -1,51 +1,33 @@
-import { useReducer } from 'react'
+import { useTodos } from '../../hooks/useTodos'
 import { TodoAdd } from './TodoAdd'
 import { TodoList } from './TodoList'
-import { todoReducer } from './todoReducer'
-
-const initialState = [
-  {
-    id: new Date().getTime(),
-    description: 'Recolectar la piedra del Alma',
-    done: false
-  },
-  {
-    id: new Date().getTime() - 1,
-    description: 'Recolectar la piedra del Poder',
-    done: false
-  }
-]
 
 export const TodoApp = () => {
 
-  const [ todos, dispatch ] = useReducer( todoReducer, initialState )
-
-  const handleNewTodo = ( todo ) => {
-
-    const action = {
-      type: 'AddTodo',
-      payload: todo
-    }
-
-    dispatch( action )
-  }
+  const { todos, todosCount, pendingTodosCount, handleNewTodo, handleDeleteTodo, onToggleTodo } = useTodos()
 
   return (
     <>
-      <h1>TodoApp 10  <small className='fs-4 ms-5'> pendientes: 2 </small></h1>
+      <h1>TodoApp { todosCount }  
+        <small className='fs-4 ms-5'> pendientes: { pendingTodosCount } </small>
+      </h1>
       <hr />
 
       <div className='row'>
 
         <div className='col-7'>
-          <TodoList todos={ todos }/>
+          <TodoList 
+            todos={ todos } 
+            onDeleteTodo={ handleDeleteTodo }
+            onToggleTodo={ onToggleTodo }
+          />
         </div>
 
         <div className='col-5'>
           <h4>Agregar TODO</h4>
           <hr />
 
-          <TodoAdd onNewTodo={handleNewTodo} />
+          <TodoAdd onNewTodo={ handleNewTodo } />
         </div>
 
       </div>
